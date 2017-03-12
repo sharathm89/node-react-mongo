03739480614386f50e8ea39e9426a58db9b094f1
Sharath <sharathm@suventure.in>
2017-03-12 17:53:39 +0530

    updated the constants

--- app.js
+++ app.js
@@ -5,27 +5,26 @@ var MongoClient = require('mongodb').MongoClient;
 var expressGraphql = require('express-graphql');
 var Schema = require('./server/schema').default;
 var cors = require('cors');
+var packageFile = require('./package.json');
 
 var db = require('./server/common/database');
 var app = express();
-var corsOptions = {
-  origin: 'http://localhost:4000'
-}
+var corsOptions = { origin: 'http://localhost:' + packageFile.config.CLIENT_PORT };
 
-db.connect('mongodb://localhost/reactmongo', function(err)
-//db.connect('mongodb://reactmongo.cloudapp.net/reactmongo', function(err)
+
+db.connect(packageFile.config.MONGO_LOCAL_URL, function(err)
 {
     if (err) 
     {
-      console.log('Unable to connect to Mongo.')
+      console.log('Unable to connect to Mongo')
       process.exit(1);
     } 
     else 
     {
-        console.log("Connected to MongoDB Server");
-        app.listen(5000, function() 
+        console.log("Connected to MongoDB Server : " + packageFile.config.MONGO_LOCAL_URL);
+        app.listen(packageFile.config.SERVER_PORT, function() 
         {
-            console.log('Listening on port 5000...');
+            console.log('Listening on port : ' + packageFile.config.SERVER_PORT);
 
             app.use(cors(corsOptions));
             app.use('/graphql', expressGraphql(
